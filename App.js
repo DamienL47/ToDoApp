7cba1f13449885ebf9bd451f5aaf679f1f1cd002
DamienL47 <damien.lataste@lapiscine.pro>
2024-04-02 08:53:50 +0200

    mapping array todo

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import backgroundImage from "./assets/background.png";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
 import { Header } from "./components/Header/Header";
 import { CardToDo } from "./components/CardToDo/CardToDo";
+import { useState } from "react";
 
 const TODO_LIST = [
   { id: 1, title: "Appeler maman", isCompleted: true },
   { id: 2, title: "Appeler papa", isCompleted: false },
   { id: 3, title: "Appeler mami", isCompleted: false },
   { id: 4, title: "Appeler papi", isCompleted: true },
+  { id: 5, title: "Appeler tati", isCompleted: false },
 ];
 
 export default function App() {
+  const [todoList, setTodoList] = useState(TODO_LIST);
+
+  function renderTodoList() {
+    return TODO_LIST.map((todo) => (
+      <View style={s.cardItem} key={todo.id}>
+        <CardToDo todo={todo} />
+      </View>
+    ));
+  }
   return (
     <>
       <SafeAreaProvider>
@@ -22,9 +33,7 @@ export default function App() {
           </View>
 
           <View style={s.body}>
-            {TODO_LIST.map((todo) => {
-              return <CardToDo todo={todo} />;
-            })}
+            <ScrollView>{renderTodoList()}</ScrollView>
           </View>
         </SafeAreaView>
       </SafeAreaProvider>
